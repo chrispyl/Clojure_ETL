c5a8ebd0a93ebdb777e4f30b9cdb33df85f45233
chrispyl <pylianidis@gmail.com>
2018-09-10 16:40:05 +0300

    cpu and memory load statistics functions

--- src/lambdawerks/core.clj
+++ src/lambdawerks/core.clj
@@ -1,7 +1,8 @@
 (ns lambdawerks.core
 	(:require [lambdawerks.xml-handling :refer [read-xml]]
 			 [lambdawerks.db-handling :refer [example-select example-select-2]]
-			 [korma.core :as kcore])
+			 [korma.core :as kcore]
+			 [lambdawerks.load-statistics :refer [memory-usage cpu-usage]])
   (:gen-class))
 
   
